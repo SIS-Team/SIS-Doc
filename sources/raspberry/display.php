3383f29c467a209df9b69dbac7f605aac6fd55b8
overflowerror <mail@overflowerror.com>
2014-03-31 19:42:10 +0200

    some more attachements, even if empty

---
+++ sources/raspberry/display.php
@@ -0,0 +1,37 @@
+<?php
+	include("../../config.php");
+	include(ROOT_LOCATION . "/modules/monitors/Main.php");
+
+	$monitor = getMonitorByName($_GET['name']);
+
+	if (!$monitor)
+		die("none");
+
+	if ($monitor->displayMode == "permanent Ein")
+		die("true");
+	else if ($monitor->displayMode == "permanent Aus")
+		die("false");
+	else {
+	
+		// monitor off on sat and sun
+		if (date("N") >= 6)
+			die("true");
+	
+		$on = $monitor->startTime;
+		$off = $monitor->endTime;
+		$on = $on % (24 * 60 * 60);
+		$off = $off % (24 * 60 * 60);
+		
+		$now = time();
+		
+		$now += date("I") * 60 * 60;
+		
+		$now = $now % (24 * 60 * 60);
+
+		if ($now > $on && $now < $off)
+			die ("true");
+		else
+			die ("false");
+	}	
+
+?>
