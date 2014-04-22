992f1ee7a504bddc34f3ab835c72f9d5884f30ff
overflowerror <mail@overflowerror.com>
2014-04-22 16:03:57 +0200

    12 new lines

---
+++ sources/base/Connect.php
@@ -0,0 +1,11 @@
+<?php
+	/* /modules/general/Connect.php
+	 * Autor: Buchberger Florian
+	 * Beschreibung:
+	 *	Stellt Verbindung zum MySQL-Server her.
+	 */
+	include_once(ROOT_LOCATION . "/modules/general/MySQLpassword.php");
+	$connection = mysql_connect($host, $user, $passwd);
+	mysql_select_db('sis');
+	echo mysql_error();
+?>
