e7ed31ffb930841bcc4aa1d58981d61d4d4ec775
Marco Handle <marco@handle-mail.at>
2014-04-22 15:24:17 +0200

    großteil fertig :)

--- sources/input/form.php
+++ sources/input/form.php
@@ -107,19 +107,19 @@ function form_lesson($field,$content,$hashGenerator)
 $zeile=0;
 printf("<table>\n");	//Tabellen Tag auf
 do{
-	if(($zeile)%7==0){
-		$zeile1=1;
-		$hour=(($zeile)/7)+1;
-		$visibility="style=\"display:tabel-row\"";
-	}
-	else{
-		$zeile1=0;
-		$hour=floor((($zeile)/7)+1);
-		$visibility="style=\"display:none\"";
-	}
+if(($zeile)%7==0){
+	$zeile1=1;
+	$hour=(($zeile)/7)+1;
+	$visibility="style=\"display:tabel-row\"";
+}
+else{
+	$zeile1=0;
+	$hour=floor((($zeile)/7)+1);
+	$visibility="style=\"display:none\"";
+}
 
-	$rowID="id=\"visibleRow".($zeile+1-(($hour-1)*7)).$hour."\"";
-	$stuff=array('split'=>1,'length'=>1);	
+$rowID="id=\"visibleRow".($zeile+1-(($hour-1)*7)).$hour."\"";
+$stuff=array('split'=>1,'length'=>1);	
 	
 if($content!=""){
 	unset($index);			
@@ -153,63 +153,63 @@ printf("<tr %s %s>\n",$rowID,$visibility);	//Zeilen 	Tag auf
  			//Für jeden Eintrag im Array field einmal diese Schleife durchlaufen
 			 $hashGenerator->printForm();
 
-		foreach($field as $f) {	
-			if(isset($index)){						//Wenn Content mitgeliefert wird und der Typ des aktuellen Inputs kein DropDown, dann Content austauschen
-				$f[5]=$stuff[$f[0]];					//Content des aktuellen Inputs austauschen, Name des Array-Indizes ist der name des Inputs
-				$edit = "readonly";
-			}	
-			else
-				$edit = "";			
-			
-			if($f[2] == "text") {
-				printf("<td>%s <input type=\"text\" name=\"%s\" size=\"%spx\" value=\"%s\" %s >\n",	//Textbox erstellen
-						$f[1], $f[0], $f[3], $f[5], $f[6]);
-			}
-			else if($f[2] == "hidden") {
-				printf("<td><input type=\"hidden\" name=\"%s\" value=\"%s\">\n",			//Versteckten Typ erstellen
-						$f[0], $f[5]);
+foreach($field as $f) {	
+	if(isset($index)){						//Wenn Content mitgeliefert wird und der Typ des aktuellen Inputs kein DropDown, dann Content austauschen
+		$f[5]=$stuff[$f[0]];					//Content des aktuellen Inputs austauschen, Name des Array-Indizes ist der name des Inputs
+		$edit = "readonly";
+	}	
+	else
+		$edit = "";			
+	
+	if($f[2] == "text") {
+		printf("<td>%s <input type=\"text\" name=\"%s\" size=\"%spx\" value=\"%s\" %s >\n",	//Textbox erstellen
+				$f[1], $f[0], $f[3], $f[5], $f[6]);
+	}
+	else if($f[2] == "hidden") {
+		printf("<td><input type=\"hidden\" name=\"%s\" value=\"%s\">\n",			//Versteckten Typ erstellen
+				$f[0], $f[5]);
+	
+if($zeile1==1){
+	printf("<td>Teilung: <input type=\"text\" name=\"%s\" id=\"%s\" size=\"2px\" value=\"%s\" onchange=\"javascript:changeText(%s);\" %s>\n",	//Textbox erstellen  			
+		"visibilityText".$hour,"visibilityText".$hour,$stuff['split'],$hour,$edit);
+	printf("<td><input type=\"button\" size=\"4px\" value=\"OK\" onclick=\"javascript:Visibility(%s)\" >\n",	//Textbox erstellen  			
+		$hour);  						
+	printf("<td>Stunde: <input type=\"text\" name =\"hour\" size=\"3px\" value=\"%s\" readonly >\n",	//Textbox erstellen  			
+		$hour);
+	printf("<td>L&auml;nge: <input type=\"text\" name=\"length\" id=\"%s\" size=\"5px\" value=\"%s\" onchange=\"javascript:visibleHours(%s);\" %s>\n",	//Textbox erstellen
+		"visibleHour".$hour, $stuff['length'], $hour,$edit);
+}
+else{
+	printf("<td><input type=\"hidden\" name=\"%s\" size=\"2px\" value=\"1\" >\n",	//Textbox erstellen  			
+		"visibilityText".$hour);
+	printf("<td><td>\n");	  						
+	printf("<td><input type=\"hidden\" name =\"hour\" size=\"3px\" value=\"%s\" readonly >\n",	//Textbox erstellen  			
+		$hour);
+	printf("<script type=\"text/javascript\">changeText(%s);</script>",$hour);
+}
+printf("<td><input type=\"hidden\" name =\"day\" value=\"%s\" >\n",	//Textbox erstellen  			
+	$_POST['day']);
+printf("<td><input type=\"hidden\" name =\"class\" value=\"%s\" >\n",	//Textbox erstellen  			
+	$_POST['class']);								
+	}
+	else if($f[2] == "dropdown") {												//Dropdown Menü erstellen
 
-				if($zeile1==1){
-					printf("<td>Teilung: <input type=\"text\" name=\"%s\" id=\"%s\" size=\"2px\" value=\"%s\" onchange=\"javascript:changeText(%s);\" %s>\n",	//Textbox erstellen  			
-  							"visibilityText".$hour,"visibilityText".$hour,$stuff['split'],$hour,$edit);
-					printf("<td><input type=\"button\" size=\"4px\" value=\"OK\" onclick=\"javascript:Visibility(%s)\" >\n",	//Textbox erstellen  			
-  							$hour);  						
-  					printf("<td>Stunde: <input type=\"text\" name =\"hour\" size=\"3px\" value=\"%s\" readonly >\n",	//Textbox erstellen  			
-  							$hour);
-  					printf("<td>L&auml;nge: <input type=\"text\" name=\"length\" id=\"%s\" size=\"5px\" value=\"%s\" onchange=\"javascript:visibleHours(%s);\" %s>\n",	//Textbox erstellen
-						"visibleHour".$hour, $stuff['length'], $hour,$edit);
-  				}
-  				else{
-  					printf("<td><input type=\"hidden\" name=\"%s\" size=\"2px\" value=\"1\" >\n",	//Textbox erstellen  			
-  							"visibilityText".$hour);
-					printf("<td><td>\n");	  						
-  					printf("<td><input type=\"hidden\" name =\"hour\" size=\"3px\" value=\"%s\" readonly >\n",	//Textbox erstellen  			
-  							$hour);
-  					printf("<script type=\"text/javascript\">changeText(%s);</script>",$hour);
-  				}
-  					printf("<td><input type=\"hidden\" name =\"day\" value=\"%s\" >\n",	//Textbox erstellen  			
-  							$_POST['day']);
-					printf("<td><input type=\"hidden\" name =\"class\" value=\"%s\" >\n",	//Textbox erstellen  			
-  							$_POST['class']);								
-			}
-			else if($f[2] == "dropdown") {												//Dropdown Menü erstellen
-
-				printf("<td>%s <input value=\"%s\" autocomplete=\"off\" size=\"%s\" list=\"%s\" name=\"%s\" %s></td>\n",
-						$f[1], $f[5], $f[3], $f[0], $f[0], $f[6]);			
-			}
-		}
-		if($zeile1==1)
-			form_savedelete(false);
-		else{
-			printf("<td></td>\n");
-			form_savedelete(true);
-		}
+		printf("<td>%s <input value=\"%s\" autocomplete=\"off\" size=\"%s\" list=\"%s\" name=\"%s\" %s></td>\n",
+				$f[1], $f[5], $f[3], $f[0], $f[0], $f[6]);			
+	}
+}
+if($zeile1==1)
+	form_savedelete(false);
+else{
+	printf("<td></td>\n");
+	form_savedelete(true);
+}
 	printf("</form>\n");
  	printf("</tr>\n");
  	if(($zeile+1-(($hour-1)*7))==7)
  		printf("<script type=\"text/javascript\">Visibility(%s);</script>",$hour);
  	$zeile+=1;
-  	}while($zeile<112);
+}while($zeile<112);
    	
 printf("</table>\n");
 }
