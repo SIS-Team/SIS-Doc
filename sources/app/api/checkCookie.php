966c5e21cc0faf8213b96f210668e2520a725a3d
Hiiias <mx5595@gmail.com>
2014-04-26 14:06:06 +0200

    update

---
+++ sources/app/api/checkCookie.php
@@ -0,0 +1,7 @@
+<?php
+	include("../../config.php");
+	include(ROOT_LOCATION . "/modules/general/SessionManager.php");
+	if ($_SESSION['loggedIn']) {
+		echo "window.location.href='menu.html';";
+	}	
+?>
